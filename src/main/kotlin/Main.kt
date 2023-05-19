439a5b71f4fa5f0dd8c074e331f20e9639df1aa8
Varun Saxena <varunkanu2000@gmail.com>
2023-05-19 14:55:34 +0530

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+val name: String="Nate"
+var greeting: String="hello"
+fun main() {
+    println(name)
+    println(greeting)
+
+   var name1: String ="Varun"
+    greeting="hi"
+    println(greeting)
+    println(name1)
+
+}
